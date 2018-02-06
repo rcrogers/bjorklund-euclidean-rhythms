e076981432ebea37e8a0f36bf1c1cc4a273a12d0
rcrogers <teukros@gmail.com>
2018-02-06 12:01:24 -0500

    Rhythm class

--- e.rb
+++ e.rb
@@ -2,4 +2,4 @@ require_relative 'src/bjorklund_euclid'
 
 k = ARGV[0].to_i
 n = ARGV[1].to_i
-puts "E(#{k},#{n})=[#{E(k,n)}]"
+puts E(k,n).inspect
--- src/bjorklund_euclid.rb
+++ src/bjorklund_euclid.rb
@@ -1,12 +1,12 @@
-def E(k, n)
-  return "" if n == 0 || k == 0
+def euclid(k, n)
+  return [] if n == 0 || k == 0
 
   bins = []
   remainders = []
-  k.times { |i| bins[i] = ['x'] }
-  (n-k).times { |i| remainders[i] = ['.'] }
+  k.times { |i| bins[i] = [true] }
+  (n-k).times { |i| remainders[i] = [false] }
 
-  return bins.flatten.join if n == k
+  return bins.flatten if n == k
 
   loop do
     new_remainders = []
@@ -27,5 +27,37 @@ def E(k, n)
     break unless remainders.size > 1
   end
 
-  return (bins + remainders).flatten.join
+  return (bins + remainders).flatten
+end
+
+Rhythm = Struct.new :weight, :pattern, :position do
+
+  def initialize(weight, *args)
+    super weight, euclid(*args), 0
+  end
+
+  def tick!
+    pattern[position].tap do
+      self.position += 1
+      self.position = position % pattern.length
+    end
+  end
+
+  def at_start?
+    position == 0
+  end
+
+end
+
+def combine(sequence_length, *rhythms)
+  [].tap do |steps|
+    step = 0
+    loop do
+      ticks = rhythms.select &:tick!
+      step += ticks.map(&:weight).sum
+      step = step % sequence_length
+      steps.push ticks.any? ? step : nil
+      break if step == 0 and rhythms.all? &:at_start?
+    end
+  end.map { |n| n + 1 if n }
 end
